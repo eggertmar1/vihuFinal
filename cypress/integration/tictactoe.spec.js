4d9e395f285e1d8ed669b4a64da6602a14f6b6c5
Eggert Eggertsson <eggerte19@ru.is>
2022-03-24 16:58:54 +0000

    tests added for validating names

--- cypress/integration/tictactoe.spec.js
+++ cypress/integration/tictactoe.spec.js
@@ -6,6 +6,26 @@ describe("#️⃣ Tic Tac Toe", () => {
     cy.visit("localhost:3000/");
   });
 
+  it("should should be required to add names to start game", () => {
+    cy.getBySel("start-game").click();
+    cy.get("input:invalid").should("have.length", 2);
+  });
+
+  it("should should be required to add both names to start game", () => {
+    cy.getBySel("player1").type("Player 1");
+    cy.getBySel("start-game").click();
+    cy.get("input:invalid").should("have.length", 1);
+    cy.get("input:valid").should("have.length", 1);
+  });
+
+  it("should have validate when both names are filled out", () => {
+    cy.getBySel("player1").type("player1");
+    cy.getBySel("player2").type("player2");
+    cy.getBySel("start-game").click();
+    cy.get("input:invalid").should("have.length", 0);
+    cy.get("input:valid").should("have.length", 2);
+  });
+
   // should test for player1 to win
   it("should test for player1 to win", () => {
     cy.getBySel("player1").type("player1");
@@ -51,5 +71,18 @@ describe("#️⃣ Tic Tac Toe", () => {
     cy.getBySel("cell-6").click();
     cy.getBySel("cell-8").click();
     cy.getBySel("draw").should("contain", "draw");
+  });
+  it("Should test for player2 to make a move", () => {
+    cy.getBySel("player1").type("player1");
+    cy.getBySel("player2").type("player2");
+    cy.getBySel("start-game").click();
+    cy.getBySel("cell-0").click();
+    cy.getBySel("cell-1").click();
+    cy.getBySel("cell-2").click();
+    cy.getBySel("cell-4").click();
+    cy.getBySel("cell-3").click();
+    cy.getBySel("cell-7").click();
+    cy.getBySel("cell-5").click();
+    cy.getBySel("winner").should("contain", "player2");
   });
 });
